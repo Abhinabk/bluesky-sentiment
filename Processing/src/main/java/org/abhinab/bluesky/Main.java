0bf5327e6f4c0ed624123f1b473e830db617ebd7
Abhinabk <abhinabk123@gmail.com>
2026-08-11 14:15:38 +0000

    Add Initial Kstreams consumer to process the data Fix bug in upstream producer (stale topic)

--- Processing/src/main/java/org/abhinab/bluesky/Main.java
+++ Processing/src/main/java/org/abhinab/bluesky/Main.java
@@ -1,7 +1,60 @@
 package org.abhinab.bluesky;
 
+import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_OFFSET_RESET_CONFIG;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //configure property
+        Properties streamProps = new Properties();
+        streamProps.setProperty(StreamsConfig.APPLICATION_ID_CONFIG,"bluesky-processing-v1");
+        streamProps.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        streamProps.setProperty(StreamsConfig.consumerPrefix(AUTO_OFFSET_RESET_CONFIG),"earliest");
+//        streamProps.setProperty("schema.registry.url","http://localhost:8081");
+
+        // TODO: All this config can be repalced with StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG
+        // build the avro serializer
+        GenericAvroSerde valueSerde = new GenericAvroSerde();
+        //configure
+        valueSerde.configure(
+                Map.of("schema.registry.url","http://localhost:8081"),
+                false
+        );
+
+        //build the stream
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, GenericRecord> stream = builder.stream("posts.raw",
+                Consumed.with(Serdes.String(), valueSerde));
+        stream.foreach((key,value)-> System.out.println(value));
+
+        //create the kafak stream instance
+        KafkaStreams kafakaStreams = new KafkaStreams(builder.build(),streamProps);
+        CountDownLatch latch = new CountDownLatch(1);
+        Runtime.getRuntime().addShutdownHook(new Thread(()->{
+            kafakaStreams.close();
+            latch.countDown();
+        }));
+        kafakaStreams.start();
+        try {
+            System.out.println("Starting kafka stream ...");
+            latch.await();
+        } catch (InterruptedException e){
+            System.out.println("Interrupted stream ...");
+            Thread.currentThread().interrupt();
+        }
     }
 }
--- TODO.md
+++ TODO.md
@@ -3,10 +3,24 @@
 # Flatten the events generated form bluesky 
 - the events are nested and doesn't fit the schema, need to flatten them and keep only 
   the important ones
-- Now i have introduced a processing layer before topic is sent to the broker by defualt kafka has 
-    At-Least_once delivery semantics so duplicates may occur (may need to think about Exactly-Once ot Atmost once
+- Now I have introduced a processing layer before topic is sent to the broker by default kafka has 
+    At-Least-once delivery semantics so duplicates may occur (may need to think about Exactly-Once ot Almost once
   for my use case )   enabled idempotency to use Exactly-Once semantics 
 # Create the Schema Registry 
 - the producer encodes the key/values using th uuid generated form the Schema Registry
 - the consumer/processor can decode the key/value by referring the uuid with corresponding schema
 - missing modules need to be added [SchemaRegistry,FastAvro] 
+
+# Create the kafka stream consumer 
+- kstreams connects to the bootstrap server and uses the schema registry to get the schema 
+- uses the avro deserializer to the read the stream of events 
+- filters the stream to only posts mentioning specific tracked brands, and
+performs sentiment analysis then transforms the survived posts enriched with 
+sentiment to JSON and write them to another topic `posts.enriched`. 
+
+will change `ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest"` to latest
+> In previous `post.raw` I was working with JSON after which without deleting the \
+> topic I started to work with avro format which caused `Unknown magic byte!` error \
+> in consumer side which could only fix form the producer side (deleting the topic and 
+> re-building the messages)
+
